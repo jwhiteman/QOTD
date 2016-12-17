8ec4dba56d24dbe81456e6e3f9f69ed124dfaae2
Jim Whiteman <jwhiteman@uplandsoftware.com>
2016-12-17 12:39:15 -0800

    Add benchmark

--- lib/qotd/client.rb
+++ lib/qotd/client.rb
@@ -3,34 +3,53 @@ module Qotd
   module Client
     extend self
 
-    def make_request(config: CONFIG, request: request)
+    def make_request(config: CONFIG, request: request, num_requests: 1)
       socket = Socket.new(:INET, :STREAM)
       addr   = Socket.pack_sockaddr_in(config.port, config.host)
 
       socket.connect(addr)
 
-      socket.write(request)
+      num_requests.times do
+        socket.write(request)
 
-      response = socket.readpartial(config.chunk)
+        @response = socket.readpartial(config.chunk)
 
-      header, body = response.split("\r\n")
+        yield @response if block_given?
+      end
+
+      header, body = @response.split("\r\n")
 
       OpenStruct.new({header: header, body: body})
     rescue Errno::ECONNREFUSED
+      yield "ECONNREFUSED" if block_given?
       socket.close
 
-      sleep 0.10
       retry
     rescue Errno::ECONNRESET
+      yield "ECONNRESET" if block_given?
+      socket.close
+
+      sleep 0.05
+      retry
+    rescue Errno::EPIPE
+      yield "EPIPE" if block_given?
       socket.close
 
       retry
-    rescue Errno::EPIPE
+    rescue Errno::EPROTOTYPE
+      yield "EPROTOTYPE" if block_given?
+      socket.close
+
+      retry
+    rescue Errno::ETIMEDOUT
+      yield "ETIMEDOUT" if block_given?
       socket.close
 
       retry
     ensure
-      socket.close
+      socket.close rescue nil
     end
+    alias_method :make_requests, :make_request
+
   end
 end
--- spec/qotd_spec.rb
+++ spec/qotd_spec.rb
@@ -30,35 +30,35 @@ describe Qotd do
     end
 
     it "serves quote request" do
-      response = client.make_request(request:  "GET quote linus\r\n")
+      response = client.make_request(config: config, request:  "GET quote linus\r\n")
 
       expect(response.header).to eq("OK: quote linus")
       expect(linus_quotes).to include(response.body)
     end
 
     it "serves author requests" do
-      response = client.make_request(request:  "GET authors\r\n")
+      response = client.make_request(config: config, request:  "GET authors\r\n")
 
       expect(response.header).to eq("OK: authors")
       expect(response.body).to eq(authors)
     end
 
     it "serves version requests" do
-      response = client.make_request(request:  "GET version\r\n")
+      response = client.make_request(config: config, request:  "GET version\r\n")
 
       expect(response.header).to eq("OK: version")
       expect(response.body).to eq(version)
     end
 
     it "returns an error response for invalid resource requests" do
-      response = client.make_request(request:  "FLOOZ flimflam\r\n")
+      response = client.make_request(config: config, request:  "FLOOZ flimflam\r\n")
 
       expect(response.header).to eq("FAIL: INVALID REQUEST")
       expect(response.body).to be_nil
     end
 
     it "returns an error response for invalid resource requests" do
-      response = client.make_request(request:  "GET quote johndoe\r\n")
+      response = client.make_request(config: config, request:  "GET quote johndoe\r\n")
 
       expect(response.header).to eq("FAIL: INVALID AUTHOR ID")
       expect(response.body).to be_nil
